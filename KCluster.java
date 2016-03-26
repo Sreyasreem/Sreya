2f7a48cb5544335cc1427c42c49564bfd9c65243
Sreyasreem <mandals@mail.gvsu.edu>
2016-03-26 18:56:52 -0400

    Added files via upload

---
+++ KCluster.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class KCluster{
+   public static void main(String[] args){
+    
+    String s;
+    Scanner scan = new Scanner(System.in); 
+     
+    s = scan.nextLine();
+
+   System.out.println("The string is "+s);
+
+  }
+}
